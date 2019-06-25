7fdd1f64e878e98ad5d7b36bedf211b207fd3b99
joelsprague <42770723+joelsprague@users.noreply.github.com>
2019-06-25 15:46:48 -0500

    Initial creation

---
+++ README.txt
@@ -0,0 +1,4 @@
+This was a rather simple project I put together because I was curious about oversubscription ratios on my VMware lab at home.
+Obviously, you will need to change the IP for vCenter to match the IP of your vCenter host.  You will also need PowerCLI installed.
+
+Other than that, it should simply run and output information on any cluster it finds in vCenter that the supplied credentials have permissions to.
---
+++ oversubscription.ps1
@@ -0,0 +1,59 @@
+﻿$vcenter = "192.168.0.1"
+Connect-VIServer $vcenter
+
+$TotalNumvCPUs = 0
+$TotalCPUCores = 0
+$TotalRAM = 0
+$TotalvRAM = 0
+
+Foreach ($Cluster in (Get-Cluster |Sort Name)){
+Write-Host ""
+Write-Host ""
+Write-Host “Cluster: $($Cluster.name)“
+Foreach ($ESXHost in ($Cluster |Get-VMHost |Sort Name)){
+Write-Host “———-“
+Write-Host “ Host: $($ESXHost.name)“
+Write-Host ""
+$CPUCores = $null
+$CPUCores = ($ESXHost).NumCPU
+$RAM = $null
+$RAM = [math]::Round(($ESXHost).MemoryTotalGB)
+$TotalCPUCores += $CPUCores
+$TotalRAM += $RAM
+ 
+Write-Host “ Physical Cores:                      $CPUCores“
+Foreach ($VM in ($ESXHost |Get-VM | Where-Object{$_.powerstate -eq 'PoweredOn'})){
+$HostNumvCPUs += ($VM).NumCpu
+}
+Foreach ($VM in ($ESXHost |Get-VM | Where-Object{$_.powerstate -eq 'PoweredOn'})){
+$HostNumvRAM += ($VM).MemoryGB
+}
+Write-Host “ Number of vCPU on host:              $($HostNumvCPUs)“
+$HostOversubscription = [math]::Round($HostNumvCPUs/$CPUCores,2)
+$RAMOversubscription = [math]::Round($HostNumvRAM/$RAM,2)
+
+Write-Host " CPU Oversubscription ratio:          $($HostOversubscription):1"
+Write-Host ""
+Write-Host " Physical Memory in GB:               $RAM"
+Write-Host " Total Virtual Memory on Host in GB:  $HostNumvRAM"
+Write-Host " RAM Oversubscription ratio:          $($RAMOversubscription):1"
+$TotalNumvCPUs += $HostNumvCPUs
+$TotalvRAM += $HostNumvRAM
+$HostNumvCPUs = 0
+$HostNumvRAM = 0
+}
+$CPUOversubscription = [math]::Round($TotalNumvCPUs/$TotalCPUCores,2)
+$RAMOversubscription = [math]::Round($TotalvRAM/$TotalRAM,2)
+Write-Host ""
+Write-Host “———-“
+Write-Host "Number of Physical Cores in $($Cluster.name):       $TotalCPUCores"
+Write-Host “Number of vCPU in $($Cluster.name):                 $TotalNumvCPUs“
+Write-Host "CPU Oversubscription ratio on cluster: $($CPUOversubscription):1"
+Write-Host ""
+Write-Host "Total Physical RAM in $($Cluster.name):             $TotalRAM"
+Write-Host "Total vRAM in $($Cluster.name):                     $TotalvRAM"
+Write-Host "RAM Oversubscription ratio on cluster: $($RAMOversubscription):1"
+Write-Host “———-“
+Write-Host “”
+$TotalNumvCPUs = 0
+}
